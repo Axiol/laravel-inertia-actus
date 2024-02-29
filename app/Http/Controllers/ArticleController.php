07db129ab0b9bdc1f6db86237aab814ae4d90102
Arnaud Delante <contact@arnaud.ninja>
2024-02-29 23:41:32 +0100

    Get back to real data

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -11,27 +11,27 @@ class ArticleController extends Controller
 {
   public function flux(): \Inertia\Response
   {
-//    $response = Http::get(env('BFF_URL').'/articles?_sort[majorUpdatedAt]=desc&_limit=30&platform=WEB&clientId=a7db15a0-5b1c-11eb-8188-2f4e3503a1f3');
-//    $articles = $response->json();
-//    $articles = $articles['data'];
+    $response = Http::get(env('BFF_URL').'/articles?_sort[majorUpdatedAt]=desc&_limit=30&platform=WEB&clientId=a7db15a0-5b1c-11eb-8188-2f4e3503a1f3');
+    $articles = $response->json();
+    $articles = $articles['data'];
 
-    $mockData = [
-      [
-        "id" => '11333607',
-        "title" => "The Voice Belgique : qui de Diogo ou Jacques a remporté son duel ?",
-      ],
-      [
-        "id" => '11337236',
-        "title" => "La saga \"Y a-t-il un flic\" aura une suite avec Liam Neeson",
-      ],
-      [
-        "id" => '11337278',
-        "title" => "Un deepfake vocal de Joe Biden a tenté de perturber la primaire démocrate : l'auteur identifié",
-      ],
-    ];
+//    $mockData = [
+//      [
+//        "id" => '11333607',
+//        "title" => "The Voice Belgique : qui de Diogo ou Jacques a remporté son duel ?",
+//      ],
+//      [
+//        "id" => '11337236',
+//        "title" => "La saga \"Y a-t-il un flic\" aura une suite avec Liam Neeson",
+//      ],
+//      [
+//        "id" => '11337278',
+//        "title" => "Un deepfake vocal de Joe Biden a tenté de perturber la primaire démocrate : l'auteur identifié",
+//      ],
+//    ];
 
     return Inertia::render('Article/Flux', [
-      'articles' => $mockData,
+      'articles' => $articles,
     ]);
   }
 
