821d07fe16319243b958771987627139a616bf68
Arnaud Delante <contact@arnaud.ninja>
2024-03-22 00:28:15 +0100

    Add shadcn/ui

--- resources/js/Layouts/Default.tsx
+++ resources/js/Layouts/Default.tsx
@@ -1,4 +1,6 @@
 import { useRemember } from '@inertiajs/react'
+import React from 'react';
+import { Button } from "@/Components/ui/button"
 
 const DefaultLayout = ({ children }) => {
   const [counter, setCounter] = useRemember(0);
@@ -7,7 +9,7 @@ const DefaultLayout = ({ children }) => {
     <>
       <header className='max-w-screen-md mx-auto mb-5'>
         <h1 className='text-3xl font-bold underline'>PDT RTBF Actus</h1>
-        <button onClick={() => setCounter(counter + 1)}>Counter: {counter}</button>
+        <Button onClick={() => setCounter(counter + 1)}>Counter: {counter}</Button>
       </header>
       <main className='max-w-screen-md mx-auto'>
         {children}
